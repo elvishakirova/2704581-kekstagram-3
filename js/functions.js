5e83604e90bbab28ab25ccc953b4d0d41f831814
Elvira Shakirova <elvira.uxdesigner@gmail.com>
2026-08-04 20:13:02 +0500

    Исправляет ошибки в функциях второго модуля

--- js/functions.js
+++ js/functions.js
@@ -1,35 +1,29 @@
- function stringLength (string, number) {
-  const length = string.length;
-  return (length < number) ? true : (length === number) ? true : (length > number) ? false : false;
-}
-
+const checkStringLength = (string, number) => string.length <= number;
 
 function isPalindrome (string) {
-  const normalizedString = string.replaceAll(" ", "").toUpperCase();
-  console.log(normalizedString);
+  const normalizedString = string.replaceAll(' ', '').toUpperCase();
   let newString = '';
 
   for (let i = normalizedString.length - 1; i >= 0; i--) {
     newString += normalizedString[i];
   }
 
-  console.log(newString);
-  return newString === normalizedString ? true : false;
+  return newString === normalizedString;
 }
 
 
 function extractingInteger (parameter) {
   let positiveString = '';
-  let normalizedParameter = parameter.toString();
+  const normalizedParameter = parameter.toString();
 
   for (let i = 0; i < normalizedParameter.length; i++) {
-    const parsedChar = parseInt(normalizedParameter[i]);
+    const parsedChar = parseInt(normalizedParameter[i], 10);
     if (!isNaN(parsedChar)) {
       positiveString += normalizedParameter[i];
     }
   }
 
-  const positiveInteger = parseInt(positiveString);
+  const positiveInteger = parseInt(positiveString, 10);
   if (isNaN(positiveInteger)) {
     return NaN;
   } else {
