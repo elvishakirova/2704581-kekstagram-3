da513d62dc16ddbb78eb87c5cccad8a7a18db529
Elvira Shakirova <elvira.uxdesigner@gmail.com>
2026-09-09 21:23:26 +0500

    Исправляет валидаторы и добавляет удаление листенеров

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,3 +1,6 @@
+const COMMENT_MAX_LENGTH = 140;
+const HASHTAG_MAX_COUNT = 5;
+
 const imageUploadForm = document.querySelector('.img-upload__form');
 const imageUploadInput = imageUploadForm.querySelector('.img-upload__input');
 const imageEditOverlay = imageUploadForm.querySelector('.img-upload__overlay');
@@ -12,76 +15,124 @@ const pristine = new Pristine(imageUploadForm, {
   errorClass: 'form__item--invalid',
   successClass: 'form__item--valid',
   errorTextParent: 'img-upload__field-wrapper',
-  errorTextTag: 'span',
+  errorTextTag: 'div',
   errorTextClass: 'form__error',
 });
 
-const validateComment = (value) => value.length <= 140;
+const validateComment = (value) => value.length <= COMMENT_MAX_LENGTH;
 
 pristine.addValidator(
   commentField,
   validateComment,
-  'Комментарий не может быть длиннее 140 символов',
+  `Комментарий не может быть длиннее ${COMMENT_MAX_LENGTH} символов`,
 );
 
-const validateHashtags = (value) => {
+const getHashtags = (value) => {
   const hashtags = value.split(/\s+/).filter((hashtag) => hashtag !== '');
-  const hashtagPattern = /^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/;
+
   const normalizedHashtags = hashtags.map((hashtag) => hashtag.toLowerCase());
   const uniqueHashtags = new Set(normalizedHashtags);
 
-  if (normalizedHashtags.length !== uniqueHashtags.size) {
-    return false;
-  }
+  return { hashtags, normalizedHashtags, uniqueHashtags };
+};
 
-  if (hashtags.length > 5) {
-    return false;
-  }
+const validateHashtagsFormat = (value) => {
+  const hashtagPattern = /^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/;
+  const { hashtags } = getHashtags(value);
 
   for (const hashtag of hashtags) {
     if (!hashtagPattern.test(hashtag)) {
       return false;
     }
   }
-  return true;
 
+  return true;
+};
+
+const validateHasgtagsCount = (value) => {
+  const { uniqueHashtags } = getHashtags(value);
+
+  if (uniqueHashtags.size > HASHTAG_MAX_COUNT) {
+    return false;
+  }
+
+  return true;
+};
+
+const validateHashtagsDoubleness = (value) => {
+  const { normalizedHashtags, uniqueHashtags } = getHashtags(value);
+
+  if (normalizedHashtags.length !== uniqueHashtags.size) {
+    return false;
+  }
+
+  return true;
 };
 
 pristine.addValidator(
   hashtagField,
-  validateHashtags,
-  'Неверный формат хэштега',
+  validateHashtagsFormat,
+  'Хэштег должен начинаться с символа #, содержать только буквы и цифры, и быть не длиннее 20 символов',
+  10,
 );
 
-const openImageUploadForm = () => {
-  imageEditOverlay.classList.remove('hidden');
-  document.body.classList.add('modal-open');
-};
+pristine.addValidator(
+  hashtagField,
+  validateHasgtagsCount,
+  `Хэштегов не может быть больше ${HASHTAG_MAX_COUNT}`,
+  5,
+);
 
-imageUploadInput.addEventListener('change', openImageUploadForm);
+pristine.addValidator(
+  hashtagField,
+  validateHashtagsDoubleness,
+  'Хэштеги не должны повторяться',
+  1,
+);
+
+let controller;
 
 const closeImageUploadForm = () => {
   imageEditOverlay.classList.add('hidden');
   document.body.classList.remove('modal-open');
   imageUploadForm.reset();
   pristine.reset();
+  controller.abort();
 };
 
-closeButton.addEventListener('click', closeImageUploadForm);
+const openImageUploadForm = () => {
+  controller = new AbortController();
+  const { signal } = controller;
 
-imageUploadForm.addEventListener('submit', (evt) => {
-  const isValid = pristine.validate();
-  if (!isValid) {
-    evt.preventDefault();
-  }
-});
+  imageEditOverlay.classList.remove('hidden');
+  document.body.classList.add('modal-open');
 
-document.addEventListener('keydown', (evt) => {
-  if (
-    document.activeElement !== commentField &&
-    document.activeElement !== hashtagField &&
-    evt.key === 'Escape'
-  ) {
-    closeImageUploadForm();
-  }
-});
+  closeButton.addEventListener('click', closeImageUploadForm, { signal });
+
+  imageUploadForm.addEventListener(
+    'submit',
+    (evt) => {
+      const isValid = pristine.validate();
+      if (!isValid) {
+        evt.preventDefault();
+      }
+    },
+    { signal },
+  );
+
+  document.addEventListener(
+    'keydown',
+    (evt) => {
+      if (
+        document.activeElement !== commentField &&
+        document.activeElement !== hashtagField &&
+        evt.key === 'Escape'
+      ) {
+        closeImageUploadForm();
+      }
+    },
+    { signal },
+  );
+};
+
+imageUploadInput.addEventListener('change', openImageUploadForm);
